2c73864c301f86b023efd917f9530c53f0d3fa13
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-04 11:20:07 -0500

    Now developping the methods and making sure they work

--- residential_controller.py
+++ residential_controller.py
@@ -1,8 +1,3 @@
-# Residential Controller
-callButtonID = 1
-elevatorID = 1
-floorRequestButtonID = 1
-
 # Column
 class Column:
     def __init__(self, id, status, amountOfFloors, amountOfElevators):
@@ -12,10 +7,27 @@ class Column:
         self.amountOfElevators = amountOfElevators
         self.callButtonList = []
         self.elevatorList = []
-        
-        # i = 1
-        # while i < self.amountOfFloors:
-
+         
+        for number in range(self.amountOfFloors):
+            buttonFloor = 1
+            buttonID = 1 
+            if buttonFloor < self.amountOfFloors:
+                callButton = CallButton(buttonID, 'off', buttonFloor, 'up')
+                self.callButtonList.append(callButton)
+                buttonID += 1
+            if buttonFloor > 1:
+                callButton = CallButton(buttonID, 'off', buttonFloor, 'down')
+                self.callButtonList.append(callButton)
+                buttonID += 1
+            buttonFloor += 1
+            
+        for number in range(self.amountOfElevators):
+            elevatorID = 1
+            elevator = Elevator(elevatorID, 'idle', self.amountOfFloors, 1)
+            self.elevatorList.append(elevator)
+            elevatorID += 1
+            
+            
 # Elevator
 class Elevator:
     def __init__(self, id, status, amountOfFloors, currentFloor):
@@ -28,6 +40,36 @@ class Elevator:
         self.floorRequestButtonList = []
         self.floorRequestList = []
         
+        for number in range(self.amountOfFloors):
+            floorNumber = 1
+            floorRequestButtonID = 1
+            floorButton = FloorRequestButton(floorRequestButtonID, 'off', floorNumber)
+            self.floorRequestButtonList.append(floorButton)
+            floorNumber += 1
+            floorRequestButtonID += 1
+            
+    def move(self):
+        while len(self.floorRequestList) != 0:
+            destination = self.floorRequestList[0]
+            self.status = 'moving'
+            if self.currentFloor < destination:
+                self.direction = 'up'
+                while self.currentFloor < destination:
+                    self.currentFloor += 1
+            elif self.currentFloor > destination:
+                self.direction = 'down'
+                while self.currentFloor > destination:
+                    self.currentFloor -= 1
+            self.status = 'idle'
+            self.floorRequestList.pop(0)
+        
+    def sortFloorRequestList(self):
+        if self.direction == 'up':
+            sorted(self.floorRequestList, key=int)
+        else:
+            sorted(self.floorRequestList, key=int, reverse=True)
+            
+
 # Call Button
 class CallButton:
     def __init__(self, id, status, floor, direction):
@@ -35,7 +77,7 @@ class CallButton:
         self.status = status
         self.floor = floor
         self.direction = direction
-        
+
 # Floor Request Button
 class FloorRequestButton:
     def __init__(self, id, status, floor):
@@ -48,3 +90,20 @@ class Doors:
     def __init__(self, id, status):
         self.id = id
         self.status = status
+        
+        
+        
+#----------------------------------------------------------------// Testing //--------------------------------------------------------------------------
+
+# testColumn = Column(1, 'online', 10, 2)            
+# print(len(testColumn.callButtonList))
+# print(testColumn.callButtonList[0].status)
+# print(len(testColumn.elevatorList))
+# print(testColumn.elevatorList)
+# print(testColumn.__dict__)
+
+testElevator = Elevator(1, 'lol', 5, 1)
+testElevator.direction = 'down'
+testElevator.floorRequestList = [1, 4, 5, 7]
+testElevator.sortFloorRequestList()
+print(testElevator.floorRequestList)
