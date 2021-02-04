346d00ce5436a8f8cfc2c9deb7d9c95e05452397
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-03 22:14:00 -0500

    Created all the classes

--- residential_controller.py
+++ residential_controller.py
@@ -10,7 +10,41 @@ class Column:
         self.status = status
         self.amountOfFloors = amountOfFloors
         self.amountOfElevators = amountOfElevators
+        self.callButtonList = []
+        self.elevatorList = []
         
+        # i = 1
+        # while i < self.amountOfFloors:
 
+# Elevator
+class Elevator:
+    def __init__(self, id, status, amountOfFloors, currentFloor):
+        self.id = id
+        self.status = status
+        self.amountOfFloors = amountOfFloors
+        self.currentFloor = currentFloor
+        self.direction = None
+        self.door = Doors(id, 'closed')
+        self.floorRequestButtonList = []
+        self.floorRequestList = []
+        
+# Call Button
+class CallButton:
+    def __init__(self, id, status, floor, direction):
+        self.id = id
+        self.status = status
+        self.floor = floor
+        self.direction = direction
+        
+# Floor Request Button
+class FloorRequestButton:
+    def __init__(self, id, status, floor):
+        self.id = id
+        self.status = status
+        self.floor = floor
 
-print('test')
+# Doors
+class Doors:
+    def __init__(self, id, status):
+        self.id = id
+        self.status = status
